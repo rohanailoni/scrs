703b1e3e8d7c140aea4567b22c0173cbc0034818
Macbook <macbook@administrators-MacBook-Pro.local>
2021-05-27 11:02:42 +0400

    configured gunicorn

--- scrs1/settings.py
+++ scrs1/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT=os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- scrs1/wsgi.py
+++ scrs1/wsgi.py
@@ -1,4 +1,16 @@
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+"""
+WSGI config for scrs1 project.
 
-application = Cling(get_wsgi_application())
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrs1.settings')
+
+application = get_wsgi_application()
