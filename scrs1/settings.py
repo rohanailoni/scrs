7cefeaaff6623639b6adba055db320817e7b7419
Macbook <macbook@administrators-MacBook-Pro.local>
2021-05-28 08:24:06 +0400

    my name is rohan

--- scrs1/settings.py
+++ scrs1/settings.py
@@ -135,7 +135,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aileni.rohan@gmail.com'
-EMAIL_HOST_PASSWORD ='Sunnybunny@9989'
+EMAIL_HOST_USER = '*********'
+EMAIL_HOST_PASSWORD ='*********'
 
 django_heroku.settings(locals())
